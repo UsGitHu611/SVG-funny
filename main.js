0fa6f9685fbce4f234a4f8096fc49b7d4ee94fde
Vladislav <vladislavtihonov790@gmail.com>
2024-04-04 23:44:35 +0300

    hmm Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   main.js
    	modified:   style.css

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const TIMER_DIGITS = document.querySelector('.timer');
 const TIMER_CLOCK = document.querySelector('#timer');
 const START_DIGITS_TIMER = document.querySelector('#start');
 const RESET_DIGITS_TIMER = document.querySelector('#stop');
+const LOG_CONTAINER_CLOCK = document.querySelector('.clock-logs');
+const LOG_CONTAINER_DIGITS = document.querySelector('.digits-logs');
 
 let StartDigTimer;
 let StartCloclTimer;
@@ -17,8 +19,9 @@ let m = 0;
 let h = 0;
 
 
+
 const INTERVAL_CLOCK_TIMER = () => {
-    SECOND_ARROW.setAttribute('transform', `rotate(${i} 250 250)`)
+    SECOND_ARROW.setAttribute('transform', `rotate(${i} 250 250)`);
     MINUTES_ARROW.setAttribute('transform', `rotate(${k} 250 250)`);
     j === 12 ? (k+=30, j=0) : j++;
     i+=30;
@@ -30,6 +33,30 @@ const INTERVAL_DIGITS_TIMER = () => {
     m === 59 && (h++, m=0);   
 };
 
+const ADD_LOGS_TIMER_DIGITS = (logContainer, ...params) => {
+    const [h, m, s] = params;
+    logContainer.insertAdjacentHTML('beforeend',`
+        <li>
+        <code>
+            ${ !h 
+                ? JSON.stringify({ hour : h, minutes : m, second : s },null, 2) 
+                : JSON.stringify({ minutes : m, second : s }, null, 2)}
+        <code/>
+        </li>
+    `);
+};
+
+const ADD_LOGS_TIMER_CLOCK = (logContainer, ...params) => {
+    const [j, k] = params;
+    logContainer.insertAdjacentHTML('beforeend',`
+        <li>
+        <code>
+            ${JSON.stringify({ minutes : k - 25, second : j * 5 }, null, 2)}}
+        <code/>
+        </li>
+    `);
+};
+
 
 TIMER_CLOCK.addEventListener('click', () => {
     console.log(isStartClockTimer);
@@ -37,7 +64,10 @@ TIMER_CLOCK.addEventListener('click', () => {
     isStartClockTimer ? (
         StartCloclTimer = clearInterval(StartCloclTimer),
         StartCloclTimer = setInterval(INTERVAL_CLOCK_TIMER,1000) 
-    ) : ( clearInterval(StartCloclTimer) )
+    ) : ( 
+        clearInterval(StartCloclTimer),
+        ADD_LOGS_TIMER_CLOCK(LOG_CONTAINER_CLOCK, j, k)
+    )
 })
 
 START_DIGITS_TIMER.addEventListener('click', () => {
@@ -46,6 +76,7 @@ START_DIGITS_TIMER.addEventListener('click', () => {
 })
 
 RESET_DIGITS_TIMER.addEventListener('click', () => {
+    ADD_LOGS_TIMER_DIGITS(LOG_CONTAINER_DIGITS,h,m,s)
     TIMER_DIGITS.textContent = '00:00:00';
     s = 0; m = 0; h = 0;
     clearInterval(StartDigTimer);
