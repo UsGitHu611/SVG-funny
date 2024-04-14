37ac4176669e7d31b7b979740bae5a29bf381e14
Vladislav <vladislavtihonov790@gmail.com>
2024-04-14 05:22:06 +0300

    add glass and percent counting

--- loadCircle/main.js
+++ loadCircle/main.js
@@ -7,7 +7,7 @@ let INTERVAL = null;
 BUTTON_TO_START_ANIMATION.addEventListener('click', () => {
     BUTTON_TO_RESET_ANIMATION.classList.remove('reset-animation');
     SPIN_FOR_ANIMATION.classList.add('main');
-
+    
     INTERVAL = setInterval(() => {
         PERCENT_LOADING.textContent = `${parseInt(PERCENT_LOADING.textContent) + 10}%`;
 
@@ -24,5 +24,6 @@ BUTTON_TO_RESET_ANIMATION.addEventListener('click', (event) => {
     SPIN_FOR_ANIMATION.classList.remove('main');
     event.currentTarget.classList.add('reset-animation');
     PERCENT_LOADING.textContent = '0%';    
+    clearInterval(INTERVAL);
 });
 
