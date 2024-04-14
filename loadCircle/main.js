084ba0635d7704705af82bcf72deb0aca1e96d38
Vladislav <vladislavtihonov790@gmail.com>
2024-04-14 05:14:40 +0300

    add glass and percent counting

--- loadCircle/main.js
+++ loadCircle/main.js
@@ -1,13 +1,28 @@
 const BUTTON_TO_START_ANIMATION = document.querySelector('.btn');
 const BUTTON_TO_RESET_ANIMATION = document.querySelector('.reset');
 const SPIN_FOR_ANIMATION = document.querySelector('#main');
+const PERCENT_LOADING = document.querySelector('.percent');
+let INTERVAL = null;
 
 BUTTON_TO_START_ANIMATION.addEventListener('click', () => {
-    BUTTON_TO_RESET_ANIMATION.classList.remove('reset-animation')
-    SPIN_FOR_ANIMATION.classList.add('main')
+    BUTTON_TO_RESET_ANIMATION.classList.remove('reset-animation');
+    SPIN_FOR_ANIMATION.classList.add('main');
+
+    INTERVAL = setInterval(() => {
+        PERCENT_LOADING.textContent = `${parseInt(PERCENT_LOADING.textContent) + 10}%`;
+
+        if(PERCENT_LOADING.textContent === '100%') {
+            SPIN_FOR_ANIMATION.classList.remove('main');    
+            clearInterval(INTERVAL);
+        }
+    }, 900);
+
 });
 
+
 BUTTON_TO_RESET_ANIMATION.addEventListener('click', (event) => {
-    SPIN_FOR_ANIMATION.classList.remove('main')
+    SPIN_FOR_ANIMATION.classList.remove('main');
     event.currentTarget.classList.add('reset-animation');
+    PERCENT_LOADING.textContent = '0%';    
 });
+
